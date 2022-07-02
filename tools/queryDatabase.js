85c25fc12fb66e506bcc32dc8a33c68d34de0ce5
HectorGit <hectorandres.pv@gmail.com>
2022-07-02 13:25:17 -0700

    change the database connection

--- tools/queryDatabase.js
+++ tools/queryDatabase.js
@@ -1,3 +1,21 @@
+/* Copyright (c) 2018-2020, Inventu Research, Inc. & Ergonomyx Technologies Canada,
+ * Inc.  ALL RIGHTS RESERVED.
+ *
+ * This copyright notice is Copyright Management Information and is included to
+ * protect this work and deter copyright infringement. This code contains the
+ * confidential information of, and is the sole property of, Inventu Research,
+ * Inc.  ("IR"), Ergonomyx Technologies Canada, Inc. (“ETC”) and its licensors
+ * and shall not be used, reproduced, copied, disclosed, published,
+ * distributed, sold, leased, lent, altered, transmitted in any form or by any
+ * means, or otherwise exploited for any purpose other than the limited
+ * purpose(s) for which it is furnished except with the written permission of
+ * IR or ETC. This notice may not be removed without the written permission of
+ * IR or ETC, and must be reproduced, without alteration, on each authorized
+ * copy hereof. Any such unauthorized removal, reproduction or alteration is a
+ * violation of federal law.
+ *
+ */
+
 /* queryDatabase.js
  *
  * Import this module and use the exported function to make queries to the
@@ -9,17 +27,18 @@
 // Import postgres pool module and connection pool
 require('dotenv').config()
 const Pool = require('pg').Pool
+
+if(process.env.NODE_ENV == "production"){
+  ssl_value = {
+    rejectUnauthorized: false
+  }
+}
+else{
+  ssl_value = false
+}
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: process.env.REQUIRE_SSL.toLowerCase() == "true"
-  
-  // ssl : false
-
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
-
-  ssl: false
+  ssl: ssl_value
 
 })
 
